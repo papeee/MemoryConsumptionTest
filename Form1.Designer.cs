608a072a8cbc6e7bd226d0ef4c74beeb1a361e88
pjrautiv <pasi.j.rautiainen@student.jyu.fi>
2024-04-22 21:50:53 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace MemoryConsumptionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(78, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(433, 165);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(141, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(141, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Memory Consumption Test 10000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 294);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "MemoryUsageTest / Pasi Rautiainen";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryConsumptionTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            TestStringConsumption();
+            TestStringBuilderConsumption();
+        }
+
+        private static long currentMemoryUsage = 0;
+        private static long AppMemoryUsage = 0;
+
+        /// <summary>
+        /// Memory consumption test of string manipulation
+        /// </summary>
+        private void TestStringConsumption()
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            long usedMemory = currentProcess.PrivateMemorySize64;
+            //currentMemoryUsage = usedMemory;
+            AppMemoryUsage = usedMemory;
+            //label2.Text = "Applicatiom: "+ usedMemory.ToString();
+
+            string s = "a";
+            for (int i = 0; i < 10000; i++)
+            {
+                if (i > 0)
+                    s += i + "b"; // Creates a new string
+            }
+            currentProcess = Process.GetCurrentProcess();
+            usedMemory = currentProcess.PrivateMemorySize64;
+            //usedMemory = usedMemory;
+            currentMemoryUsage = usedMemory - AppMemoryUsage;
+            label3.Text = "String: " + currentMemoryUsage.ToString() + " bytes";
+        }
+
+        /// <summary>
+        /// Memory consumption test of StringBuilder
+        /// </summary>
+        private void TestStringBuilderConsumption()
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            //long usedMemory = currentProcess.PrivateMemorySize64;
+
+            //label2.Text = usedMemory.ToString();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("a");
+            for (int i = 1; i < 10000; i++)
+            {
+                sb.Append(i).Append("b"); // Append to one string
+            }
+            currentProcess = Process.GetCurrentProcess();
+            long usedMemory = currentProcess.PrivateMemorySize64  - currentMemoryUsage - AppMemoryUsage;
+            label4.Text = "StringBuilder: " + usedMemory.ToString() + " bytes";
+            
+        }
+    }
+}
